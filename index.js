1389b4580aa21bd49bff5c0e2f48ce14cd056b2b
Vladimir Vancea <vladimir.vancea@roche.com>
2019-07-23 18:49:32 +0300

    let's make the build fail

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function(req, res) {
-  res.send("hey world");
+  res.send("hello world");
 });
 
 app.listen(port, err => {
